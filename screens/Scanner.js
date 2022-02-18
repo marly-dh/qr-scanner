fed1e055ff2ef43ee038ffdd3335069c355aaea3
MarlydeHaard <61316126+MarlydeHaard@users.noreply.github.com>
2022-02-18 13:56:10 +0100

    Alert toegevoegd
    Elke keer als je scant komt er een alert die vraagt of je wilt inchecken gebaseerd op of je al ingecheckt bent of niet

--- screens/Scanner.js
+++ screens/Scanner.js
@@ -1,49 +1,67 @@
 import { getDatabase, ref, onValue} from 'firebase/database';
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 import { getAuth } from "firebase/auth";
 import { getFirestore, doc, setDoc, updateDoc, getDoc } from "firebase/firestore"
 
 const auth = getAuth();
-
-async function storeData(location, token) {
-  const user = auth.currentUser;
-  const d = new Date();
-  const date = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
-  const db = getDatabase();
-  const fs = getFirestore();
-  const fsReference = doc(fs, 'registraties', date);
-  const tokenRefrence = ref(db, '/token');
-  const userSnap = await getDoc(fsReference);
-
-  onValue(tokenRefrence, (snapshot) => {
-    if (snapshot.val() === token) {
-
-      if (userSnap.exists() && userSnap.data()[user.displayName] !== undefined) {
-        updateDoc(fsReference, {
-          [user.displayName + '.checkOut']: d.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
-        })
-      } else {
-        setDoc(fsReference, {
-          [user.displayName]: {
-            checkIn: d.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
-            checkOut: "",
-            account: user.email,
-            location: location
-          }
-        });
-      }
-
-    }
-  }, (errorObject) => {
-    console.log('The read failed: ' + errorObject.name);
-  });
-}
+const d = new Date();
 
 const ScannerScreen = () => {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
+
+
+  const checkOut = (ref, user) => {
+    updateDoc(ref, {
+      [user.displayName + '.checkOut']: d.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'})
+    })
+  };
+
+
+  const checkIn = (ref, location, user) => {
+    setDoc(ref, {
+      [user.displayName]: {
+        checkIn: d.toLocaleTimeString(navigator.language, {hour: '2-digit', minute:'2-digit'}),
+        checkOut: "",
+        account: user.email,
+        location: location
+      }
+    });
+  };
+
+
+  const alertHandler = async (data) => {
+    const user = auth.currentUser;
+    const date = d.getDate()+'-'+(d.getMonth()+1)+'-'+d.getFullYear();
+    const db = getDatabase();
+    const fs = getFirestore();
+    const fsReference = doc(fs, 'registraties', date);
+    const tokenRefrence = ref(db, '/token');
+    const userSnap = await getDoc(fsReference);
+
+    onValue(tokenRefrence, (snapshot) => {
+      if (snapshot.val() === data.key) {
+
+        if (userSnap.exists() && userSnap.data()[user.displayName] !== undefined) {
+          Alert.alert('Check uit', 'Weet je zeker dat je wilt uitchecken?', [
+            { text: 'annuleer', onPress: () => {}, style: 'cancel' },
+            { text: 'ja', onPress: () => {checkOut(fsReference, user)}}
+          ]);
+        } else {
+          Alert.alert('Check in', 'Weet je zeker dat je wilt inchecken?', [
+            { text: 'annuleer', onPress: () => {}, style: 'cancel' },
+            { text: 'ja', onPress: () => {checkIn(fsReference, data.location, user)}}
+          ]);
+        }
+
+      } else {
+        alert("Incorrecte QR code!")
+      }
+    });
+  };
+
 
   useEffect(() => {
     (async () => {
@@ -52,12 +70,13 @@ const ScannerScreen = () => {
     })();
   }, []);
 
+
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     let values = JSON.parse(data);
-    storeData(values.location, values.key);
-    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+    alertHandler(values);
   };
+
 
   if (hasPermission === null) {
     return <Text>Requesting for camera permission</Text>;
@@ -66,6 +85,7 @@ const ScannerScreen = () => {
     return <Text>No access to camera</Text>;
   }
 
+
   return (
     <View style={styles.container}>
       <View style={styles.cameraContainer}>
@@ -73,10 +93,10 @@ const ScannerScreen = () => {
           onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
-        {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+        {scanned && <Button title={'scan opnieuw'} onPress={() => setScanned(false)} />}
       </View>
       <View style={styles.bottom}>
-        <Button title="Sign Out" style={styles.button} onPress={() => getAuth().signOut()} />
+        <Button title="log uit" style={styles.button} onPress={() => getAuth().signOut()} />
       </View>
     </View>
   );
